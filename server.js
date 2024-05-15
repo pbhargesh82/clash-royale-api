311c9c6d55e453cb7e10f84f9d423d5da22020ac
Bhargesh Patel <pbhargesh82@aol.com>
2024-05-15 14:42:22 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+console.log(process.env.PORT)
+console.log(port)
+const token = process.env.CLASH_ROYALE_API_TOKEN;
+console.log(token)
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Example route to fetch Clash Royale data
+app.get('/clashroyale', (req, res) => {
+    axios.get('https://api.clashroyale.com/v1/players/%23UQJQYQR/upcomingchests', {
+      headers: {
+          'Authorization': process.env.CLASH_ROYALE_API_TOKEN
+      }
+    })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error fetching Clash Royale data');
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
