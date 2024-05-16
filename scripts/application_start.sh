377834e934112012a6abb745279a30fb3bac4c04
Bhargesh Patel <pbhargesh82@aol.com>
2024-05-16 13:38:10 +0530

    changes in script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ubuntu/clash-royale-api/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'pm2 restart server.js' >> /home/ubuntu/clash-royale-api/deploy.log
-pm2 restart server.js >> /home/ubuntu/clash-royale-api/deploy.log
+pm2 restart nodejs-express-app >> /home/ubuntu/clash-royale-api/deploy.log
